b46894d8e6fd053b9cdae59979e1ad25ffdfe707
Yi-Ru Lan <a78945612385238@gmail.com>
2022-06-12 01:53:14 +0800

    Add Mutation

---
+++ src/resolvers/Mutation.ts
@@ -0,0 +1,20 @@
+import { Context } from '../index';
+
+interface PostCreateArgs {
+  title: string;
+  content: string;
+}
+
+export const postCreate = async (
+  _: any,
+  { title, content }: PostCreateArgs,
+  { prisma }: Context,
+) => {
+  prisma.post.create({
+    data: {
+      title,
+      content,
+      authorId: 1,
+    },
+  });
+};
