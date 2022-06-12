054a492d2da79153abb197611018e71a456fc70b
Yi-Ru Lan <a78945612385238@gmail.com>
2022-06-12 17:01:49 +0800

    Update postCreate resolver

--- src/resolvers/Mutation.ts
+++ src/resolvers/Mutation.ts
@@ -1,3 +1,4 @@
+import { Post } from '@prisma/client';
 import { Context } from '../index';
 
 interface PostCreateArgs {
@@ -5,16 +6,39 @@ interface PostCreateArgs {
   content: string;
 }
 
+interface PostPayloadType {
+  userErrors: {
+    message: string;
+  }[];
+  post: Post | null;
+}
+
 export const postCreate = async (
   _: any,
   { title, content }: PostCreateArgs,
   { prisma }: Context,
-) => {
-  prisma.post.create({
+): Promise<PostPayloadType> => {
+  if (!title || !content) {
+    return {
+      userErrors: [
+        {
+          message: 'You must provide title and content to create a post',
+        },
+      ],
+      post: null,
+    };
+  }
+
+  const post = await prisma.post.create({
     data: {
       title,
       content,
       authorId: 1,
     },
   });
+
+  return {
+    userErrors: [],
+    post,
+  };
 };
