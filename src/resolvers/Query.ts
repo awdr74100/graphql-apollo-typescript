dbf54aa3b0cc2b8b1ca4c34185f74672f705d7ba
Yi-Ru Lan <a78945612385238@gmail.com>
2022-06-17 19:45:22 +0800

    Add me resolver

--- src/resolvers/Query.ts
+++ src/resolvers/Query.ts
@@ -1,5 +1,15 @@
 import { Context } from '..';
 
+export const me = (_: any, __: any, { prisma, userInfo }: Context) => {
+  if (!userInfo) return null;
+
+  return prisma.user.findUnique({
+    where: {
+      id: userInfo.userId,
+    },
+  });
+};
+
 export const posts = (_: any, __: any, { prisma }: Context) => {
   return prisma.post.findMany({
     orderBy: [
